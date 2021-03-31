e1072d99e13062003c539c55fdc5fe8a2a05f5eb
Alex White <awhite214@gmail.com>
2021-03-31 09:35:29 -0400

    updates to JS, HTML, and CSS

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-let city = $(".city");
-let wind = $(".wind");
-let humidity = $(".humidity");
-let temp = $(".temp");
+$(document).foundation();
+const formSubmitBtn = $("#form-submit");
+const userInput = JSON.parse(localStorage.getItem("userInput"));
 
 let searchArr = [];
-let APIKey = "&appid=853047de1547a2ec23c2f40353e444e0";
-
-$(document).ready(function () {
-    renderSearchList();
-
-    $("#searchBtn").click(function (event) {
-        event.preventDefault();
-        //grab search term from input search field
-        let searchTerm = $("#cityname").val().trim();
-        triggerSearch(searchTerm);
-    })
-
-    function triggerSearch(citySearch) {
-        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-            citySearch + APIKey;
+    
+formSubmitBtn.click( () => {
+    saveUserForm();
+  })
+  
+function saveUserForm(e) {
+    if (($("#cityName").val() == "") && ($("#citySelect").val() == "")) {
+        confirm("Error: Please make sure to choose a city!");
+    } 
+    else if ($("#cityName").val() == "") {
+        const userInput = {
+            cityName: $("#citySelect").val()
+        };
+        localStorage.setItem('userInput', JSON.stringify(userInput));
     }
+    else {
+        const userInput = {
+            cityName: $("#cityName").val()
+        };
+        localStorage.setItem('userInput', JSON.stringify(userInput));
+    }
+};
+
+function renderWeather() {
+    let wRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput.cityName + "&appid=c571dcc28202f5c8d2b8377fd87551f0";
+    let wLocal = document.getElementById("weather-today")
+    let t = document.getElementById("temperature")
+    let pic = document.getElementById("weather-pic")
+    let f = document.getElementById("forecast")
+    let img = document.createElement("img")
+    
+    fetch(wRequest)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log (data)
+        wLocal.innerHTML = "Current Weather in " + data.name
+        img.src = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+        // pic.append(img)
+        t.innerHTML = "Temperature: " + data.main.temp.toFixed() + "&degF"
+        t.append(img)
+      })
+  }
+
+  $("h3").append(userInput.cityName);
+  renderWeather();
